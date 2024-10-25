ae4fffba73ed9023e94693c01543c8c6c78a5a3a
LENT4869 <122330145+LENT4869@users.noreply.github.com>
2024-10-25 15:13:03 +0800

    Update Restart.sh

--- Restart.sh
+++ Restart.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 
-# 設定要執行的程序相對路徑
-PROGRAM="./node-2.0.1-linux-amd64"
-# 設定要運行的時間（秒）
-RUN_TIME=300  # 5分鐘 = 300秒
-# 設定重新啟動的等待時間（秒）
-RESTART_WAIT=10  # 10秒鐘
+# 檢查並關閉已有的進程
+pkill -f "./node-2.0.1-linux-amd64"
 
-while true; do
-    # 啟動程序
-    $PROGRAM --core=16 --parent-process=$$ &  # 在後台運行，並獲取其進程 ID
-    PID=$!  # 獲取最近一個後台進程的 PID
-
-    # 等待指定的運行時間
-    sleep $RUN_TIME
-
-    # 發送終止信號 (SIGINT) 到進程
-    kill -SIGINT $PID  # 這模擬 Ctrl+C
-
-    # 等待進程結束
-    wait $PID
-
-    echo "程序已停止，等待 $RESTART_WAIT 秒後重新啟動..."
-    
-    # 等待指定的重新啟動時間
-    sleep $RESTART_WAIT
-done
+# 重啟 ./node-2.0.1-linux-amd64
+cd /root/ceremonyclient/node && ./node-2.0.1-linux-amd64
