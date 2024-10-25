9ea8ae5c93edf27b1382057482723fac092da445
LENT4869 <122330145+LENT4869@users.noreply.github.com>
2024-10-25 11:31:15 +0800

    Update Restart.sh

--- Restart.sh
+++ Restart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 檢查並關閉已有的進程
-pkill -f "./node-2.0.1-linux-amd64"
+pkill -f node-
 
 # 重啟 ./node-2.0.1-linux-amd64
-cd ceremonyclient/node && ceremonyclient/node/node-2.0.1-linux-amd64 
+cd root/ceremonyclient/node && ./node-2.0.1-linux-amd64 
 
